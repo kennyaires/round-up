9e68c24f5b7ceb776a5be2c1f8d6d8c644853482
mateus <mateusmecunhe@gmail.com>
2021-03-21 07:46:00 -0300

    adding front end logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import settings_dev
-
+import math
 from os import environ
 from flask import Flask, render_template
 
@@ -7,10 +7,43 @@ app = Flask(__name__)
 app.config.from_object(settings_dev)
 
 
-@app.route('/')
-def chackout():
-    return render_template('checkout.html')
+class Produto:
+    name = ''
+    price = 0
+    quantity = 0
 
+
+
+@app.route('/')
+def checkout():
+    p1 = Produto()
+    p1.name = 'Produto 1'
+    p1.price = 10.15
+    p1.quantity = 3
+    p1.total = round(p1.price * p1.quantity, 2)
+
+    p2 = Produto()
+    p2.name = 'Produto 2'
+    p2.price = 9.87
+    p2.quantity = 2
+    p2.total = round(p2.price * p2.quantity, 2)
+
+    total_amount = p1.total + p2.total
+    round_up_donation = round(math.ceil(total_amount) - total_amount, 2)
+
+    products = [p1, p2]
+
+    
+    return render_template('checkout.html', products=products, total_amount=total_amount, round_up_donation=round_up_donation)
+
+@app.route('/confirm')
+def confirm():
+    return render_template('confirm.html')
+
+@app.route('/confirm/rounded')
+def confirm_donation():
+    # implement LinkAPI logic
+    return render_template('confirm-rounded.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=environ.get("PORT", 5000))
